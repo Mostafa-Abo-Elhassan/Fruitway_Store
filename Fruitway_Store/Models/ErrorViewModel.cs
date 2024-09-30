108e00885bc836edd7f64f81862848abac4ed5ae
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2024-10-01 00:08:28 +0200

    fruitway store MVC

---
+++ Fruitway_Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fruitway_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
